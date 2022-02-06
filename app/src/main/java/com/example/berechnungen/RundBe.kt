0c03cce176cbd5b6b5b669a473dc04e27b8b275f
darktown <tiefflieger1981@googlemail.com>
2022-02-06 19:22:20 +0100

    Berechnung angefangen zu schreiben

--- app/src/main/java/com/example/berechnungen/RundBe.kt
+++ app/src/main/java/com/example/berechnungen/RundBe.kt
@@ -2,6 +2,7 @@ package com.example.berechnungen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 
 class RundBe : AppCompatActivity() {
@@ -9,13 +10,33 @@ class RundBe : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rund_be)
 
+
+        val berechnen = findViewById<Button>(R.id.buBerechnung2)
         val innen = findViewById<EditText>(R.id.innen).text
-        val rad = innen.toString().toDouble() / 2
         val pi = kotlin.math.PI
         val boden = findViewById<EditText>(R.id.boden).text
-        //val bo = boden.toString().toDouble()
+        val hoehe = findViewById<EditText>(R.id.hoehe).text
+        val wand = findViewById<EditText>(R.id.wand).text
 
 
+        berechnen.setOnClickListener{
+
+            //berechnung des Rohres ohne Boden.
+            val hoeheohneb = hoehe.toString().toDouble() - boden.toString().toDouble()
+
+            //Durchmesser zum Radius
+            val inrad = innen.toString().toDouble() / 2
+
+            //berechnung des außenraduis
+            val ausrad = inrad + wand.toString().toDouble()
+
+
+
+
+
+
+
+        }
 
 
     }
